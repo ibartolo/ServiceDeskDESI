81f7bab13d3fef9eb58a14583eee259996757b86
ibartolo <bartolocastro@gmail.com>
2026-03-11 18:51:32 -0600

    se agrega primer template de autenticación

---
+++ ServiceDeskDESI/ServiceDeskDESIMVC/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ServiceDeskDESIMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Autenticacion()
+        { 
+            return View();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
