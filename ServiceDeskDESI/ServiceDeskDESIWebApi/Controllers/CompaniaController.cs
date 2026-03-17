41ace9818a7c4098428fc4952d62ce556e91f508
LAPTOP-TJLEOC8Q\JOSE <inocar22.15@gmail.com>
2026-03-17 14:41:45 -0600

    se creo el DbWrapperCompania y CompaniaControler

---
+++ ServiceDeskDESI/ServiceDeskDESIWebApi/Controllers/CompaniaController.cs
@@ -0,0 +1,52 @@
+﻿using ServiceDeskDESIEntities.Catalogos;
+using ServiceDeskDESIEntities.Seguridad;
+using ServiceDeskDESIWebApi.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ServiceDeskDESIWebApi.Controllers
+{
+    [RoutePrefix("api/compania")]
+    public class CompaniaController : ApiController
+    {
+        private DbWrapper dbWrapper;
+        public CompaniaController()
+        {
+            dbWrapper = new DbWrapper();
+        }
+
+        [HttpGet, Route("")]
+        public ModelResponse ObtenerCompania()
+        {
+            var result = dbWrapper.ObtenerCompania();
+            return result;
+        }
+        [HttpGet, Route("{id:long}")]
+        public ModelResponse ObtenerCompaniaPorId(long id)
+        {
+            var result = dbWrapper.ObtenerCompaniaPorId(id);
+            return result;
+        }
+
+        [HttpPost, Route("")]
+        public ModelResponse GuardarOActualizarCompania(Compania c)
+        {
+            var result = dbWrapper.GuardarOActualizarCompania(c);
+            return result;
+        }
+
+        [HttpDelete, Route("")]
+        public ModelResponse EliminarCompania(Compania c)
+        {
+            c.FechaModificacion = DateTime.Now;
+            var result = dbWrapper.EliminarCompania(c.Id, c.ModificadoPor, c.FechaModificacion.Value);
+            return result;
+        }
+
+    }
+}
+
---
+++ ServiceDeskDESI/ServiceDeskDESIWebApi/DAL/DbWrapper.Compania.cs
@@ -0,0 +1,125 @@
+﻿using ServiceDeskDESIEntities.Catalogos;
+using ServiceDeskDESIEntities.Seguridad;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace ServiceDeskDESIWebApi.DAL
+{
+    public partial class DbWrapper
+    {
+        public ModelResponse ObtenerCompania()
+        {
+            var modelResponse = new ModelResponse();
+            try
+            {
+                modelResponse.IsSuccess = true;
+                var parameters = new List<SqlParameter>();
+                var result = GetObject("ObtenerCompanias", System.Data.CommandType.StoredProcedure,
+                    parameters, new Func<System.Data.IDataReader, Compania>((reader) =>
+                    {
+                        var r = MapearPorpiedades<Compania>(reader);
+                        return r;
+                    }));
+                modelResponse.Response = result;
+
+            }
+            catch (Exception ex)
+            {
+                modelResponse.IsSuccess = true;
+                modelResponse.Message ="Companias obtenidas correctamente";
+                modelResponse.Response = ex;
+            }
+            return modelResponse;
+        }
+
+        public ModelResponse GuardarOActualizarCompania(Compania c)
+        {
+            var modelResponse = new ModelResponse();
+            try
+            {
+                var parametros = ObtenerParametrosSQL(c).ToArray();
+                var companiaId = ExecuteScalar("GuardarOActualizarCompania", CommandType.StoredProcedure, parametros);
+                c.Id = Convert.ToInt64(companiaId);
+
+                modelResponse.IsSuccess = true;
+                modelResponse.Response = c;
+                modelResponse.Message = "Compania Guardado correctamente";
+            }
+            catch (Exception ex)
+            {
+                modelResponse.IsSuccess = false;
+                modelResponse.Message = ex.Message;
+                modelResponse.Response = null;
+            }
+
+            return modelResponse;
+        }
+         public ModelResponse EliminarCompania(long id, string modificadoPor, DateTime fechaModificacion)
+
+         {
+            var modelResponse = new ModelResponse();
+            try
+            {
+                ExecuteNonQuery("EliminarCompania", CommandType.StoredProcedure, new SqlParameter[]
+                {
+                    new SqlParameter("@Id", id),
+                    new SqlParameter("@ModificadoPor", modificadoPor),
+                    new SqlParameter("@FechaModificacion", fechaModificacion)
+                });
+
+                modelResponse.IsSuccess = true;
+                modelResponse.Message = "Compania eliminado correctamente";
+                modelResponse.Response = null;
+            }
+            catch (Exception ex)
+            {
+                modelResponse.IsSuccess = false;
+                modelResponse.Message = ex.Message;
+                modelResponse.Response = null;
+            }
+
+            return modelResponse;
+         }
+        public ModelResponse ObtenerCompaniaPorId (long id)
+        {
+            var modelResponse = new ModelResponse();
+           try
+           {
+                modelResponse.IsSuccess = true;
+                var parameters = new List<SqlParameter>();
+                parameters.Add(new SqlParameter()
+                {
+                    Value = id,
+                    IsNullable = true,
+                    ParameterName = "@Id",
+                    SqlDbType = System.Data.SqlDbType.Int
+                });
+
+                var result = GetObject("ObtenerCompaniaPorId", System.Data.CommandType.StoredProcedure,
+                    parameters, new Func<System.Data.IDataReader, Compania>((reader) =>
+                    {
+                        var r = LlenarEntidad<Compania>(reader);
+                        return r;
+                    }));
+                modelResponse.Response = result;
+                modelResponse.IsSuccess = true;
+                modelResponse.Message = "Compania obtenido correctamente";
+            }
+            catch (Exception ex)
+            {
+                modelResponse.IsSuccess = false;
+                modelResponse.Message = ex.Message;
+                modelResponse.Response = null;
+            }
+
+            return modelResponse;
+
+        }
+
+    }
+
+}
