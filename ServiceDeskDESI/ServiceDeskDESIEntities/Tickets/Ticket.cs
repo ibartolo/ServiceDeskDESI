bd521cb51d0e38a2cb14db146a4a5751ca2b3123
Ivan <bartolocastro@gmail.com>
2026-04-10 19:40:14 -0600

    Se agrega funcionalidad para visualizar y generar nuevos tickets

---
+++ ServiceDeskDESI/ServiceDeskDESIEntities/Tickets/Ticket.cs
@@ -0,0 +1,19 @@
+﻿using ServiceDeskDESIEntities.Catalogos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceDeskDESIEntities.Tickets
+{
+    public class Ticket : BaseObject
+    {
+        public Area Area { get; set; }
+        public Categoria Categoria { get; set; }
+        public Categoria Subcategoria { get; set; }
+        public int Urgencia { get; set; }
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
---
+++ ServiceDeskDESI/ServiceDeskDESIMVC/DAL/HttpClientConnection.Ticket.cs
@@ -0,0 +1,101 @@
+﻿using Newtonsoft.Json;
+using ServiceDeskDESIEntities.Seguridad;
+using ServiceDeskDESIEntities.Tickets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ServiceDeskDESIMVC.DAL
+{
+    public partial class HttpClientConnection
+    {
+        public async Task<ModelResponse> ObtenerTickets()
+        {
+            var result = await RequestAsync<object>($"api/Ticket/Lista", HttpMethod.Get, null,
+                new Func<string, string>((responseString) =>
+                {
+                    return responseString;
+                }), token.Token.access_token);
+
+            var modelResponse = JsonConvert.DeserializeObject<ModelResponse>(result.ToString());
+            return modelResponse;
+        }
+
+        public async Task<ModelResponse> ObtenerTicketPorId(long id)
+        {
+            var result = await RequestAsync<object>($"api/Ticket/{id}", HttpMethod.Get, null,
+                new Func<string, string>((responseString) =>
+                {
+                    return responseString;
+                }), token.Token.access_token);
+
+            var modelResponse = JsonConvert.DeserializeObject<ModelResponse>(result.ToString());
+            return modelResponse;
+        }
+
+        public async Task<ModelResponse> GuardarOActualizarTicket(Ticket ticket)
+        {
+            MappingColumSecurity(ticket);
+
+            var result = await RequestAsync<object>($"api/Ticket", HttpMethod.Post, ticket,
+                new Func<string, string>((responseString) =>
+                {
+                    return responseString;
+                }), token.Token.access_token);
+
+            var modelResponse = JsonConvert.DeserializeObject<ModelResponse>(result.ToString());
+            return modelResponse;
+        }
+
+        public async Task<ModelResponse> EliminarTicket(Ticket ticket)
+        {
+            var result = await RequestAsync<object>($"api/Ticket", HttpMethod.Delete, ticket,
+                new Func<string, string>((responseString) =>
+                {
+                    return responseString;
+                }), token.Token.access_token);
+
+            var modelResponse = JsonConvert.DeserializeObject<ModelResponse>(result.ToString());
+            return modelResponse;
+        }
+
+        public async Task<ModelResponse> ObtenerTicketsPorArea(long areaId)
+        {
+            var result = await RequestAsync<object>($"api/Ticket/Area/{areaId}", HttpMethod.Get, null,
+                new Func<string, string>((responseString) =>
+                {
+                    return responseString;
+                }), token.Token.access_token);
+
+            var modelResponse = JsonConvert.DeserializeObject<ModelResponse>(result.ToString());
+            return modelResponse;
+        }
+
+        public async Task<ModelResponse> ObtenerTicketsPorUsuario(string creadoPor)
+        {
+            var result = await RequestAsync<object>($"api/Ticket/Usuario/{creadoPor}", HttpMethod.Get, null,
+                new Func<string, string>((responseString) =>
+                {
+                    return responseString;
+                }), token.Token.access_token);
+
+            var modelResponse = JsonConvert.DeserializeObject<ModelResponse>(result.ToString());
+            return modelResponse;
+        }
+
+        public async Task<ModelResponse> ObtenerTicketsPorUrgencia(int urgencia)
+        {
+            var result = await RequestAsync<object>($"api/Ticket/Urgencia/{urgencia}", HttpMethod.Get, null,
+                new Func<string, string>((responseString) =>
+                {
+                    return responseString;
+                }), token.Token.access_token);
+
+            var modelResponse = JsonConvert.DeserializeObject<ModelResponse>(result.ToString());
+            return modelResponse;
+        }
+    }
+}
