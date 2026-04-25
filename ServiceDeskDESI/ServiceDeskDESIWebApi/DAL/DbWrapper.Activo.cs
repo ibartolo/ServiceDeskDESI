b4128f95d9e86d3309048732b626296534560eea
LAPTOP-TJLEOC8Q\JOSE <inocar22.15@gmail.com>
2026-04-24 21:39:22 -0600

    Se Crearon Los Catalogos de Activo,TipoActivo,Modelo y Marca con sus respectivos SP, se Crearon las clases en Entidades y se avanzo con DbWrapper de dos de ellos.

---
+++ ServiceDeskDESI/ServiceDeskDESIWebApi/DAL/DbWrapper.Activo.cs
@@ -0,0 +1,124 @@
+﻿using ServiceDeskDESIEntities.Catalogos;
+using ServiceDeskDESIEntities.Seguridad;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace ServiceDeskDESIWebApi.DAL
+{
+    public partial class DbWrapper
+    {
+        public ModelResponse ObtenerTodosActivos()
+        {
+            var modelResponse = new ModelResponse();
+            try
+            {
+                var Activos = GetObjects("ObtenerActivos", CommandType.StoredProcedure, Enumerable.Empty<SqlParameter>(),
+                    new Func<IDataReader, Activo>((reader) =>
+                    {
+                        var activo = LlenarEntidad<Activo>(reader);
+                        return activo;
+                    }));
+                modelResponse.IsSuccess = true;
+                modelResponse.Response = Activos;
+                modelResponse.Message = "Activos obtenidos correctamente";
+
+            }
+            catch (Exception ex)
+            {
+                modelResponse.IsSuccess = false;
+                modelResponse.Message = ex.Message;
+                modelResponse.Response = null;
+            }
+            return modelResponse;
+
+        }
+
+        public ModelResponse GuardarOActualizarActivo(Activo a)
+        {
+            var modelResponse = new ModelResponse();
+            try
+            {
+                var parametros = ObtenerParametrosSQL(a).ToArray();
+                var activoId = ExecuteScalar("GuardarOActualizarActivo", CommandType.StoredProcedure, parametros);
+                a.Id = Convert.ToInt64(activoId);
+
+                modelResponse.IsSuccess = true;
+                modelResponse.Response = a;
+                modelResponse.Message = "Activos Guardados Correctamente";
+            }
+            catch (Exception ex)
+            {
+                modelResponse.IsSuccess = false;
+                modelResponse.Message = ex.Message;
+                modelResponse.Response = null;
+            }
+
+            return modelResponse;
+        }
+
+        public ModelResponse ObtenerActivosPorId (long id)
+        {
+            var modelResponse = new ModelResponse();
+            try
+            {
+                modelResponse.IsSuccess = true;
+                var parameters = new List<SqlParameter>();
+                parameters.Add(new SqlParameter()
+                {
+                    Value = id,
+                    IsNullable = true,
+                    ParameterName = "@Id",
+                    SqlDbType = System.Data.SqlDbType.Int
+                });
+
+                var result = GetObject("ObtenerActivoPorId", System.Data.CommandType.StoredProcedure,
+                    parameters, new Func<System.Data.IDataReader, Activo>((reader) =>
+                    {
+                        var r = LlenarEntidad<Activo>(reader);
+                        return r;
+                    }));
+                modelResponse.Response = result;
+                modelResponse.IsSuccess = true;
+                modelResponse.Message = "Compania obtenido correctamente";
+            }
+            catch (Exception ex)
+            {
+                modelResponse.IsSuccess = false;
+                modelResponse.Message = ex.Message;
+                modelResponse.Response = null;
+            }
+
+            return modelResponse;
+        }
+
+        public ModelResponse EliminarActivo(Activo a)
+        {
+            var modelResponse = new ModelResponse();
+            try
+            {
+                ExecuteNonQuery("EliminarActivo", CommandType.StoredProcedure, new SqlParameter[]
+                {
+                    new SqlParameter("@Id", a.Id),
+                    new SqlParameter("@ModificadoPor", a.ModificadoPor),
+                    new SqlParameter("@FechaModificacion",a.FechaModificacion)
+                });
+
+                modelResponse.IsSuccess = true;
+                modelResponse.Message = "Activo Eliminado Correctamente";
+                modelResponse.Response = null;
+            }
+            catch (Exception ex)
+            {
+                modelResponse.IsSuccess = false;
+                modelResponse.Message = ex.Message;
+                modelResponse.Response = null;
+            }
+
+            return modelResponse;
+        }
+    }
+}
