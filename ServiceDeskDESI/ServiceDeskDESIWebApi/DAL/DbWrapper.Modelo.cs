048aba452b0128a400221efdf7dd26012c3625c8
LAPTOP-TJLEOC8Q\JOSE <inocar22.15@gmail.com>
2026-04-28 14:40:46 -0600

    se crearon las clases Marca, Modelo,Activo Y TipoActivo en entidades

---
+++ ServiceDeskDESI/ServiceDeskDESIWebApi/DAL/DbWrapper.Modelo.cs
@@ -0,0 +1,16 @@
+﻿using ServiceDeskDESIEntities.Seguridad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ServiceDeskDESIWebApi.DAL
+{
+    public partial class DbWrapper
+    {
+        //public ModelResponse ObtenerTodosLosModelos()
+        //{
+
+        //}
+    }
+}
