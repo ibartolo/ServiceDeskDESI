e0af969387ec4878a8d7b2121aa8684d97e95f8c
LAPTOP-TJLEOC8Q\JOSE <inocar22.15@gmail.com>
2026-05-18 21:14:26 -0600

    Se sube Avance del Catalogo Sucursal(Entidades,Api,Mvc. terminado) Pendiente Vista Sucursal(Branch)

---
+++ ServiceDeskDESI/ServiceDeskDESIWebApi/Controllers/SucursalController.cs
@@ -0,0 +1,41 @@
+﻿using ServiceDeskDESIEntities.Catalogos;
+using ServiceDeskDESIEntities.Seguridad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ServiceDeskDESIWebApi.Controllers
+{
+    [RoutePrefix("api/sucursales")]
+    public class SucursalController : BaseController
+    {
+        [HttpGet, Route("List")]
+        public ModelResponse ObtenerSucursales()
+        {
+            var result = dbWrapper.ObtenerTodosLasSucursales();
+            return result;
+        }
+        [HttpGet, Route("{id:long}")]
+        public ModelResponse ObtenerSucursalesPorId(long id)
+        {
+            var result = dbWrapper.ObtenerSucursalesPorId(id);
+            return result;
+        }
+        [HttpPost, Route("Guardar")]
+        public ModelResponse GuardarActualizarSucursal(Sucursal s)
+        {
+            var result = dbWrapper.GuardarOActualizarSucursales(s);
+            return result;
+        }
+        [HttpDelete, Route("Eliminar")]
+        public ModelResponse EliminarSucursal( Sucursal s)
+        {
+            s.FechaModificacion = DateTime.Now;
+            var result = dbWrapper.EliminarSucursales(s);
+            return result;
+        }
+    }
+}
---
+++ ServiceDeskDESI/ServiceDeskDESIWebApi/DAL/DbWrapper.Sucursal.cs
@@ -0,0 +1,121 @@
+﻿using ServiceDeskDESIEntities.Catalogos;
+using ServiceDeskDESIEntities.Seguridad;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace ServiceDeskDESIWebApi.DAL
+{
+    public partial class DbWrapper
+    {
+        public ModelResponse ObtenerTodosLasSucursales()
+        {
+            var modelResponse = new ModelResponse();
+            try
+            {
+                var sucursales = GetObjects("ObtenerSucursales", CommandType.StoredProcedure, Enumerable.Empty<SqlParameter>(),
+                new Func<IDataReader, Sucursal>((reader) =>
+                 {
+                     var sucursal = LlenarEntidad<Sucursal>(reader);
+                     return sucursal;
+                 }));
+                modelResponse.IsSuccess = true;
+                modelResponse.Response = sucursales;
+                modelResponse.Message= "Sucursales obtenidos correctamente";
+            }
+            catch(Exception ex)
+            {
+                modelResponse.IsSuccess = false;
+                modelResponse.Message = ex.Message;
+                modelResponse.Response = null;
+            }
+            return modelResponse;
+        }
+
+        public ModelResponse GuardarOActualizarSucursales(Sucursal s)
+        {
+            var modelResponse = new ModelResponse();
+            try
+            {
+                var parametros = ObtenerParametrosSQL(s).ToArray();
+                var sucursalId = ExecuteScalar("GuardarOActualizarSucursal", CommandType.StoredProcedure, parametros);
+                s.Id = Convert.ToInt64(sucursalId);
+
+                modelResponse.IsSuccess = true;
+                modelResponse.Response = s;
+                modelResponse.Message = "Sucursales Guardados Correctamente";
+            }
+            catch (Exception ex)
+            {
+                modelResponse.IsSuccess = false;
+                modelResponse.Message = ex.Message;
+                modelResponse.Response = null;
+            }
+
+            return modelResponse;
+        }
+        public ModelResponse ObtenerSucursalesPorId(long id)
+        {
+            var modelResponse = new ModelResponse();
+            try
+            {
+                modelResponse.IsSuccess = true;
+                var parameters = new List<SqlParameter>();
+                parameters.Add(new SqlParameter()
+                {
+                    Value = id,
+                    IsNullable = true,
+                    ParameterName = "@Id",
+                    SqlDbType = System.Data.SqlDbType.Int
+                });
+
+                var result = GetObject("ObtenerSucursalPorId", System.Data.CommandType.StoredProcedure,
+                    parameters, new Func<System.Data.IDataReader, Sucursal>((reader) =>
+                    {
+                        var r = LlenarEntidad<Sucursal>(reader);
+                        return r;
+                    }));
+                modelResponse.Response = result;
+                modelResponse.IsSuccess = true;
+                modelResponse.Message = "Sucursal Obtenido Correctamente";
+            }
+            catch (Exception ex)
+            {
+                modelResponse.IsSuccess = false;
+                modelResponse.Message = ex.Message;
+                modelResponse.Response = null;
+            }
+
+            return modelResponse;
+        }
+        public ModelResponse EliminarSucursales (Sucursal s)
+        {
+            var modelResponse = new ModelResponse();
+            try
+            {
+                ExecuteNonQuery("EliminarSucursal", CommandType.StoredProcedure, new SqlParameter[]
+                {
+                    new SqlParameter("@Id", s.Id),
+                    new SqlParameter("@ModificadoPor", s.ModificadoPor),
+                    new SqlParameter("@FechaModificacion",s.FechaModificacion)
+                });
+
+                modelResponse.IsSuccess = true;
+                modelResponse.Message = "Sucursal Eliminado Correctamente";
+                modelResponse.Response = null;
+            }
+            catch (Exception ex)
+            {
+                modelResponse.IsSuccess = false;
+                modelResponse.Message = ex.Message;
+                modelResponse.Response = null;
+            }
+
+            return modelResponse;
+        }
+
+    }
+}
