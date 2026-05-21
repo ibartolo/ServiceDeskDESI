38aad5156902577cd2bd0e178cbc032094a1049c
LAPTOP-TJLEOC8Q\JOSE <inocar22.15@gmail.com>
2026-05-20 20:41:27 -0600

    Se sube Avanses de Empresa Entidades y API terminado(revisar metodo consultarPorRFC

---
+++ ServiceDeskDESI/ServiceDeskDESIWebApi/Controllers/EmpresaController.cs
@@ -0,0 +1,47 @@
+﻿using ServiceDeskDESIEntities.Catalogos;
+using ServiceDeskDESIEntities.Seguridad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ServiceDeskDESIWebApi.Controllers
+{
+    [RoutePrefix("api/empresas")]
+    public class EmpresaController : BaseController
+    {
+        [HttpGet, Route("List")]
+        public ModelResponse ObtenerEmpresas()
+        {
+            var result = dbWrapper.ObtenerTodasLasEmpresas();
+            return result;
+        }
+        [HttpGet, Route("{id:long}")]
+        public ModelResponse ObtenerEmpresasPorId(long id)
+        {
+            var result = dbWrapper.ObtenerEmpresasPorId(id);
+            return result;
+        }
+        [HttpGet, Route("{id:string}")]
+        public ModelResponse ObtenerEmpresasPorRFC(string rfc)
+        {
+            var result = dbWrapper.ObtenerEmpresaPorRFC(rfc);
+            return result;
+        }
+        [HttpPost, Route("")]
+        public ModelResponse GuardarOActualizarEmpresas(Empresa e)
+        {
+            var result = dbWrapper.GuardarOActualizarEmpresas(e);
+            return result;
+        }
+        [HttpDelete, Route("")]
+        public ModelResponse EliminarEmpresas(Empresa e)
+        {
+            e.FechaModificacion = DateTime.Now;
+            var result = dbWrapper.EliminarEmpresa(e.Id, e.ModificadoPor, e.FechaModificacion.Value);
+            return result;
+        }
+    }
+}
---
+++ ServiceDeskDESI/ServiceDeskDESIWebApi/DAL/DbWrapper.Empresa.cs
@@ -0,0 +1,155 @@
+﻿using ServiceDeskDESIEntities.Catalogos;
+using ServiceDeskDESIEntities.Seguridad;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace ServiceDeskDESIWebApi.DAL
+{
+    public partial class DbWrapper
+    {
+        public ModelResponse ObtenerTodasLasEmpresas ()
+        {
+            var modelResponse = new ModelResponse();
+            try
+            {
+                var empresa = GetObjects("ObtenerEmpresas", CommandType.StoredProcedure, Enumerable.Empty<SqlParameter>(),
+               new Func<IDataReader, Empresa>((reader) =>
+               {
+                   var empresas = LlenarEntidad<Empresa>(reader);
+                   return empresas;
+               }));
+                modelResponse.IsSuccess = true;
+                modelResponse.Response = empresa;
+                modelResponse.Message = "Empresas obtenidas correctamente";
+            }
+            catch (Exception ex)
+            {
+                modelResponse.IsSuccess = false;
+                modelResponse.Message = ex.Message;
+                modelResponse.Response = null;
+            }
+            return modelResponse;
+        }
+
+        public ModelResponse GuardarOActualizarEmpresas(Empresa e)
+        {
+            var modelResponse = new ModelResponse();
+            try
+            {
+                var parametros = ObtenerParametrosSQL(e).ToArray();
+                var empresaId = ExecuteScalar("GuardarOActualizarEmpresa", CommandType.StoredProcedure, parametros);
+                e.Id = Convert.ToInt64(empresaId);
+
+                modelResponse.IsSuccess = true;
+                modelResponse.Response = e;
+                modelResponse.Message = "Empresas Guardados Correctamente";
+            }
+            catch (Exception ex)
+            {
+                modelResponse.IsSuccess = false;
+                modelResponse.Message = ex.Message;
+                modelResponse.Response = null;
+            }
+
+            return modelResponse;
+        }
+        public ModelResponse ObtenerEmpresaPorRFC(string rfc)
+        {
+            var modelResponse = new ModelResponse();
+            try
+            {
+                modelResponse.IsSuccess = true;
+                var parameters = new List<SqlParameter>();
+                parameters.Add(new SqlParameter()
+                {
+                    Value = rfc,
+                    IsNullable = true,
+                    ParameterName = "@RFC",
+                    SqlDbType = System.Data.SqlDbType.Int
+                });
+
+                var result = GetObject("ObtenerEmpresaPorId", System.Data.CommandType.StoredProcedure,
+                    parameters, new Func<System.Data.IDataReader, Empresa>((reader) =>
+                    {
+                        var r = LlenarEntidad<Empresa>(reader);
+                        return r;
+                    }));
+                modelResponse.Response = result;
+                modelResponse.IsSuccess = true;
+                modelResponse.Message = "Empresa Obtenido Correctamente";
+            }
+            catch (Exception ex)
+            {
+                modelResponse.IsSuccess = false;
+                modelResponse.Message = ex.Message;
+                modelResponse.Response = null;
+            }
+
+            return modelResponse;
+        }
+        public ModelResponse ObtenerEmpresasPorId(long id)
+        {
+            var modelResponse = new ModelResponse();
+            try
+            {
+                modelResponse.IsSuccess = true;
+                var parameters = new List<SqlParameter>();
+                parameters.Add(new SqlParameter()
+                {
+                    Value = id,
+                    IsNullable = true,
+                    ParameterName = "@Id",
+                    SqlDbType = System.Data.SqlDbType.Int
+                });
+
+                var result = GetObject("ObtenerEmpresaPorId", System.Data.CommandType.StoredProcedure,
+                    parameters, new Func<System.Data.IDataReader, Empresa>((reader) =>
+                    {
+                        var r = LlenarEntidad<Empresa>(reader);
+                        return r;
+                    }));
+                modelResponse.Response = result;
+                modelResponse.IsSuccess = true;
+                modelResponse.Message = "Empresa Obtenido Correctamente";
+            }
+            catch (Exception ex)
+            {
+                modelResponse.IsSuccess = false;
+                modelResponse.Message = ex.Message;
+                modelResponse.Response = null;
+            }
+
+            return modelResponse;
+        }
+        public ModelResponse EliminarEmpresa(long id, string modificadoPor, DateTime fechaModificacion)
+        {
+            var modelResponse = new ModelResponse();
+            try
+            {
+                ExecuteNonQuery("EliminarEmpresa", CommandType.StoredProcedure, new SqlParameter[]
+                {
+                    new SqlParameter("@Id", id),
+                    new SqlParameter("@ModificadoPor", modificadoPor),
+                    new SqlParameter("@FechaModificacion", fechaModificacion)
+                });
+
+                modelResponse.IsSuccess = true;
+                modelResponse.Message = "Empresa Eliminado Correctamente";
+                modelResponse.Response = null;
+            }
+            catch (Exception ex)
+            {
+                modelResponse.IsSuccess = false;
+                modelResponse.Message = ex.Message;
+                modelResponse.Response = null;
+            }
+
+            return modelResponse;
+        }
+
+    }
+}
