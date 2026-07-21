7d53d256ccf543dd0d3c2781e0a6d437b6a50ef6
Ivan <bartolocastro@gmail.com>
2026-07-20 23:19:38 -0600

    se realiza agrega el Compañia services y se ajusta el filtrado por usuario para el catalogo de compañia

--- ServiceDeskDESI/ServiceDeskDESIWebApi/Controllers/CompaniaController.cs
+++ ServiceDeskDESI/ServiceDeskDESIWebApi/Controllers/CompaniaController.cs
@@ -1,6 +1,6 @@
 ﻿using ServiceDeskDESIEntities.Catalogos;
 using ServiceDeskDESIEntities.Seguridad;
-using ServiceDeskDESIWebApi.DAL;
+using ServiceDeskDESIWebApi.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,43 +10,67 @@ using System.Web.Http;
 
 namespace ServiceDeskDESIWebApi.Controllers
 {
+    [Authorize]
     [RoutePrefix("api/Compania")]
     public class CompaniaController : BaseController
     {
-        //private DbWrapper dbWrapper;
-        //public CompaniaController()
-        //{
-        //    dbWrapper = new DbWrapper();
-        //}
+        private readonly CompaniaService _companiaService;
 
-        [HttpGet, Route("List/{empresaId:long}")]
-        public ModelResponse ObtenerCompania(long empresaId)
+        public CompaniaController()
         {
-            var result = dbWrapper.ObtenerCompania(empresaId);
-            return result;
-        }
-        [HttpGet, Route("{id:long}/{empresaId:long}")]
-        public ModelResponse ObtenerCompaniaPorId(long id, long empresaId)
-        {
-            var result = dbWrapper.ObtenerCompaniaPorId(id,empresaId);
-            return result;
+            _companiaService = new CompaniaService();
         }
 
-        [HttpPost, Route("Guardar/{empresaId:long}")]
-        public ModelResponse GuardarOActualizarCompania(Compania c, long empresaId)
+        /// <summary>
+        /// Obtiene todas las compañías de la empresa del usuario autenticado
+        /// </summary>
+        /// <returns>Lista de compañías</returns>
+        [HttpGet, Route("List")]
+        public ModelResponse ObtenerCompanias()
         {
-            var result = dbWrapper.GuardarOActualizarCompania(c,empresaId);
+            var usuario = User.Identity.Name;
+            var result = _companiaService.ObtenerCompanias(usuario);
             return result;
         }
 
-        [HttpDelete, Route("Compania")]
-        public ModelResponse EliminarCompania(Compania c, long empresaId)
+        /// <summary>
+        /// Obtiene una compañía por su ID
+        /// </summary>
+        /// <param name="id">ID de la compañía</param>
+        /// <returns>Compañía encontrada</returns>
+        [HttpGet, Route("{id:long}")]
+        public ModelResponse ObtenerCompaniaPorId(long id)
         {
-            c.FechaModificacion = DateTime.Now;
-            var result = dbWrapper.EliminarCompania(c.Id,c.ModificadoPor,c.FechaModificacion.Value,empresaId);
+            var usuario = User.Identity.Name;
+            var result = _companiaService.ObtenerCompaniaPorId(id, usuario);
             return result;
         }
 
+        /// <summary>
+        /// Guarda o actualiza una compañía
+        /// </summary>
+        /// <param name="compania">Objeto compañía con los datos</param>
+        /// <returns>Compañía guardada con su ID actualizado</returns>
+        [HttpPost, Route("Guardar")]
+        public ModelResponse GuardarOActualizarCompania(Compania compania)
+        {
+            var usuario = User.Identity.Name;
+            var result = _companiaService.GuardarOActualizarCompania(compania, usuario);
+            return result;
+        }
+
+        /// <summary>
+        /// Elimina lógicamente una compañía
+        /// </summary>
+        /// <param name="compania">Compañía a eliminar (debe incluir Id y ModificadoPor)</param>
+        /// <returns>Resultado de la operación</returns>
+        [HttpDelete, Route("Eliminar")]
+        public ModelResponse EliminarCompania(Compania compania)
+        {
+            var usuario = User.Identity.Name;
+            compania.FechaModificacion = DateTime.Now;
+            var result = _companiaService.EliminarCompania(compania.Id, compania.ModificadoPor, compania.FechaModificacion.Value, usuario);
+            return result;
+        }
     }
 }
-
